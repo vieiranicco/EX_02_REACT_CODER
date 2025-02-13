03b1468a18765e267cccbb4b2668e8730d732808
vieiranicco <nicollas.contato@hotmail.com>
2025-02-12 21:45:26 -0300

    exAula02

---
+++ EcommerceTenis-Vieira/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css"
+export function Header() {
+	return (
+		<nav className="nav-header">
+			<h1> SHOeCOMMERCE </h1>
+			<ul>
+				<li>Início</li>
+				<li>Masculino</li>
+				<li>Feminino</li>
+				<li>Acessórios</li>
+			</ul>
+		</nav>
+	)
+}
